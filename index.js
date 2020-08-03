412f4158b1efeb90a0b2881be2913e4061396f2a
unknown <anand9412868527@gmail.com>
2020-08-02 23:53:24 -0700

    initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+
+//Load config
+// dotenv.config({path:'./config/config.env'});
+const connectDB = require('./models/db')
+connectDB()
+
+const app = express();
+app.use(morgan('dev'))
+//Handlebars
+app.engine('.hbs',exphbs({extname:'hbs'}));
+app.set('view engine','.hbs')
+
+// Static folder
+app.use(express.static())
+
+//Routes
+app.use('/',require('./routes/index'))
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT,()=>console.log(`Server  running in ${process.env.NODE_ENV} mode on port ${PORT}`))
---
+++ models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    try{
+        const conn = await mongoose.connect("mongodb+srv://anand123:1234567890@cluster0.sjivs.mongodb.net/storyapp?retryWrites=true&w=majority",{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useFindAndModify:false
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    }catch(err){
+        console.error(err);
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
