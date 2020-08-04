1c35213806e6a875d08ea12ddf0031eb9ff3910f
unknown <anand9412868527@gmail.com>
2020-08-04 10:17:57 -0700

    Final Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const morgan = require('morgan');
 const passport = require('passport')
 const session = require('express-session');
@@ -18,15 +19,33 @@ connectDB()
 
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 //for consoling
 app.use(morgan('dev'))
 
 //Handlebars Helpers
-const {formatDate} = require('./helpers/hbs')
+const {formatDate,stripTags,truncate,editIcon} = require('./helpers/hbs')
 
 //Handlebars
-app.engine('.hbs',exphbs({extname:'hbs'}));
+app.engine('.hbs',exphbs({helpers:{formatDate,stripTags,truncate,editIcon},defaultLayout:'main',extname:'hbs'}));
 app.set('view engine','.hbs')
+
+//Method Override middleware
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      console.log(req.body);
+      delete req.body._method
+      console.log("after");
+      console.log(req.body)
+      console.log(method)
+      return method
+    }
+  }))
 
 //session middleware
 app.use(session({
@@ -39,6 +58,13 @@ app.use(session({
 //Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global vars
+app.use((req,res,next) =>{
+    res.locals.user = req.user || null;
+    next()
+})
+
 
 // Static folder
 app.use(express.static(path.join(__dirname,'public')))
