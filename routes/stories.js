c94f19075c87b778faa250d7453fe5d7adba5586
unknown <anand9412868527@gmail.com>
2020-08-04 10:44:03 -0700

    Last change

--- routes/stories.js
+++ routes/stories.js
@@ -46,6 +46,24 @@ router.get('/:id',ensureAuth, async (req,res)=>{
     }
 })
 
+
+// @desc  User stories
+// @route GET /stories/user/:userId
+
+router.get('/user/:userId',ensureAuth, async (req,res)=>{
+    try {
+        const stories = await Story.find({
+            user:req.params.userId,
+            status:'public'
+        }).populate('user').lean()
+
+        res.render('stories/index',{stories})
+    } catch (error) {
+        console.error(error);
+        res.render('error/500')
+    }
+})
+
 // @desc Show all stories
 // @route GET /stories
 
