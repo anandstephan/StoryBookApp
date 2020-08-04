ed236f4539cb0712ba6b649fe31fa2f16d0bc1b8
unknown <anand9412868527@gmail.com>
2020-08-03 17:43:09 -0700

    Adding Google Authentication

--- index.js
+++ index.js
@@ -3,23 +3,45 @@ const express = require('express');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
+const passport = require('passport')
+const session = require('express-session');
 
 //Load config
 // dotenv.config({path:'./config/config.env'});
+
+//passport
+require('./config/passport')(passport);
+
 const connectDB = require('./models/db')
 connectDB()
 
 const app = express();
+
+//for consoling
 app.use(morgan('dev'))
+
+
 //Handlebars
 app.engine('.hbs',exphbs({extname:'hbs'}));
 app.set('view engine','.hbs')
 
+//session middleware
+app.use(session({
+    secret:'keyboard cat',
+    resave:false,
+    saveUninitialized:false
+}))
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static folder
-app.use(express.static())
+app.use(express.static(path.join(__dirname,'public')))
 
 //Routes
 app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000; 
 
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
 const express =require('express');
 const router = express.Router();
+const {ensureAuth,ensureGuest} = require('../middleware/auth');
 
 // @desc Login/Landing Page
 // @route GET/
 
-router.get('/',(req,res)=>{
-res.render('login')
+router.get('/',ensureGuest ,(req,res)=>{
+res.render('login',{layout:'login'})
 })
 
 // @desc Dashboard
 // @route GET/
 
-router.get('/dashboard',(req,res)=>{
+router.get('/dashboard',ensureAuth,(req,res)=>{
     res.render('dashboard')
     })
+
+// @desc Logout
+// @route GET/
+
+router.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+})
 
 module.exports = router;
